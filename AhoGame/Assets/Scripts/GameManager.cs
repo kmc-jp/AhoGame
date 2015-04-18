7be80436b5181faad59b7a066c4fed75ae002504
spi8823 <spi8823@kmc.gr.jp>
2015-04-18 13:46:04 +0900

    ちょっとすすんだ

--- AhoGame/Assets/Scripts/GameManager.cs
+++ AhoGame/Assets/Scripts/GameManager.cs
@@ -10,20 +10,32 @@ public class GameManager : MonoBehaviour
     /// 0がスタート画面、1~11がゲーム画面、12がスコア画面
     /// </summary>
     int nowStageNo = 0;
-    int stagenum = 1;
+    int stagenum = 2;
     Phase phase;
 
     List<GameObject> stagePrefabs;
 
+    PointerController pointer;
+    TextManager textManager;
+
     public void Awake()
     {
         camera = GameObject.Find("Camera").transform;
-        stagePrefabs = new List<GameObject>();
-        for(var i = 0;i < 1;i++)
-        {
-
-        }
+        LoadStages();
     }
+
+    public void LoadStages()
+    {
+        stagePrefabs = new List<GameObject>();
+        for (var i = 1; i < stagenum + 1; i++)
+        {
+            stagePrefabs.Add(Resources.Load<GameObject>("Prefabs/Stage" + i));
+        }
+
+
+    }
+
+
 
     public void Start()
     {
@@ -32,9 +44,12 @@ public class GameManager : MonoBehaviour
 
     public void Update()
     {
-
+        AcceptInput();
     }
 
+    /// <summary>
+    /// 入力の適用
+    /// </summary>
     public void AcceptInput()
     {
         if (Input.GetKeyDown(KeyCode.Return))
@@ -43,11 +58,11 @@ public class GameManager : MonoBehaviour
             {
 
             }
-            else if(0 < nowStageNo || nowStageNo < 12)
+            else if(0 < nowStageNo || nowStageNo < stagenum + 1)
             {
 
             }
-            else if(nowStageNo == 12)
+            else if(nowStageNo == stagenum + 1)
             {
 
             }
@@ -60,9 +75,11 @@ public class GameManager : MonoBehaviour
 
         if(Input.GetMouseButtonDown(0))
         {
+            Debug.Log("!!");
             if(nowStageNo == 0)
             {
-
+                StartGame();
+                phase = Phase.Score;
             }
             else if(0 <nowStageNo || nowStageNo < 12)
             {
@@ -73,6 +90,7 @@ public class GameManager : MonoBehaviour
                     case Phase.Cutting:
                         break;
                     case Phase.Score:
+                        TransStage(nowStageNo++);
                         break;
                 }
 
@@ -84,18 +102,57 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void LoadStages()
-    {
+    //前のステージ用のオブジェクトたち
+    GameObject previousStageObject;
+    StageController previousStageController;
+    string previousBackGroundImageName;
 
+    //今のステージ用のオブジェクトたち
+    GameObject nowStageObject;
+    StageController nowStageController;
+    string nowBackGroundImageName;
+
+    public void StartGame()
+    {
+        string setting = Resources.Load<TextAsset>("StageSettings/Stage1").text;
+        nowStageObject = stagePrefabs[0];
+        nowStageController = nowStageObject.GetComponent<StageController>();
+        nowBackGroundImageName = nowStageController.Initialize(setting);
+        nowStageNo++;
+    }
+
+    /// <summary>
+    /// ステージの遷移
+    /// </summary>
+    /// <param name="toNo"></param>
+    public void TransStage(int toNo)
+    {
+        previousStageObject = nowStageObject;
+        previousStageController = nowStageController;
+        previousBackGroundImageName = nowBackGroundImageName;
+
+        string setting = Resources.Load<TextAsset>("StageSettings/Stage" + toNo).text;
+        nowStageObject = stagePrefabs[toNo - 1];
+        nowStageController = nowStageObject.GetComponent<StageController>();
+        nowBackGroundImageName = nowStageController.Initialize(setting);
+
+        //フェードイン/アウト
+        if(nowBackGroundImageName != "" && previousBackGroundImageName != null && nowBackGroundImageName != previousBackGroundImageName)
+        {
+            GameObject.Find("/BackGround/" + previousBackGroundImageName).GetComponent<Animator>().SetBool("Out", true);
+            GameObject.Find("/BackGround/" + nowBackGroundImageName).GetComponent<Animator>().SetBool("In", true);
+        }
+
+        //Stage○の表示をにゅって感じで出す
     }
 
     /// <summary>
     /// 
     /// </summary>
     /// <param name="no"></param>
-    public void StageStart(int no)
+    public void StartCutting(int no)
     {
-
+        
     }
 
     public enum Phase
--- AhoGame/Assets/Scripts/StageController.cs
+++ AhoGame/Assets/Scripts/StageController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StageController : MonoBehaviour
 {
@@ -8,10 +10,55 @@ public class StageController : MonoBehaviour
     public int EndX;
     Animator animator;
 
+    string targetName;
+    int width;
+    int height;
+    bool showInAdvance;
+    int percent;
+    int textnum;
+    List<string> texts;
+
+    int nowText = -1;
 
     public void Awake()
     {
         animator = this.GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Stageの初期化
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns>Stageの背景画像ファイルの名前</returns>
+    public string Initialize(string text)
+    {
+        string[] settings = text.Split('\n');
+        targetName = settings[0].Split(',')[1];
+        width = Int32.Parse(settings[1].Split(',')[1]);
+        height = Int32.Parse(settings[2].Split(',')[1]);
+        showInAdvance = Boolean.Parse(settings[3].Split(',')[1]);
+        percent = Int32.Parse(settings[4].Split(',')[1]);
+        textnum = Int32.Parse(settings[6].Split(',')[1]);
+        texts = new List<string>();
+        for(var i = 0;i < textnum;i++)
+        {
+            string t = "";
+            for(var j = 0;j < 4;j++)
+            {
+                t += settings[7 + i * 4 + j];
+                t += "\n";
+            }
+            texts.Add(t);
+        }
+        texts.Add("");
+
+        return settings[5].Split(',')[1];
+    }
+
+    public string GetNextText()
+    {
+        nowText++;
+        return texts[nowText];
     }
 
     public void Start()
