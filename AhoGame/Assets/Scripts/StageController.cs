5e7d4dc11115735ece2077aca397ba7d1e2b83f1
spi8823 <spi8823@kmc.gr.jp>
2015-04-18 11:19:24 +0900

    にゅってなるアニメーション云々 いろんなコントローラー

---
+++ AhoGame/Assets/Scripts/StageController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageController : MonoBehaviour
+{
+    public GameObject Target;
+    public int BeginX;
+    public int EndX;
+    Animator animator;
+
+
+    public void Awake()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+    }
+
+    public void Enter()
+    {
+        animator.SetBool("Enter", true);
+        animator.SetBool("Entered", true);
+    }
+
+    public void Exit()
+    {
+        animator.SetBool("Exit", true);
+    }
+
+    public Rect GetImageRect()
+    {
+        
+        return new Rect();
+    }
+
+    public enum CutType
+    {
+        Vertical, Horizontal
+    }
+}
---
+++ AhoGame/Assets/Scripts/TextManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextManager : MonoBehaviour
+{
+    RectTransform textBoxPanelRTF;
+    Text textBox;
+
+    public void Awake()
+    {
+        Transform canvas = GameObject.Find("/Canvas").transform;
+        textBoxPanelRTF = canvas.FindChild("TextBoxPanel").transform as RectTransform;
+        textBox = textBoxPanelRTF.FindChild("TextBox").GetComponent<Text>();
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+    }
+
+    public void SetText(string text)
+    {
+        textBox.text = text;
+    }
+}
