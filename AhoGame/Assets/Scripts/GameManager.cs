167e233b5ce8342f854447be2449adae3d852f61
spi8823 <spi8823@kmc.gr.jp>
2015-04-18 11:49:29 +0900

    ステージの遷移を実装しようと準備している

--- AhoGame/Assets/Scripts/GameManager.cs
+++ AhoGame/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,12 +9,20 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// 0がスタート画面、1~11がゲーム画面、12がスコア画面
     /// </summary>
-    int stageNo = 0;
+    int nowStageNo = 0;
+    int stagenum = 1;
     Phase phase;
+
+    List<GameObject> stagePrefabs;
 
     public void Awake()
     {
         camera = GameObject.Find("Camera").transform;
+        stagePrefabs = new List<GameObject>();
+        for(var i = 0;i < 1;i++)
+        {
+
+        }
     }
 
     public void Start()
@@ -30,15 +39,15 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            if(stageNo == 0)
+            if(nowStageNo == 0)
             {
 
             }
-            else if(0 < stageNo || stageNo < 12)
+            else if(0 < nowStageNo || nowStageNo < 12)
             {
 
             }
-            else if(stageNo == 12)
+            else if(nowStageNo == 12)
             {
 
             }
@@ -51,11 +60,11 @@ public class GameManager : MonoBehaviour
 
         if(Input.GetMouseButtonDown(0))
         {
-            if(stageNo == 0)
+            if(nowStageNo == 0)
             {
 
             }
-            else if(0 <stageNo || stageNo < 12)
+            else if(0 <nowStageNo || nowStageNo < 12)
             {
                 switch(phase)
                 {
@@ -68,11 +77,25 @@ public class GameManager : MonoBehaviour
                 }
 
             }
-            else if(stageNo == 12)
+            else if(nowStageNo == 12)
             {
 
             }
         }
+    }
+
+    public void LoadStages()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="no"></param>
+    public void StageStart(int no)
+    {
+
     }
 
     public enum Phase
